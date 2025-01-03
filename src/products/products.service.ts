13d962bc30ebf687a9cc78bc809dc15f10f5a529
icaroalmeida-somma <icaro.almeida@somma-it.com>
2025-01-03 19:08:35 -0300

    feat: duplicity validations

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -7,46 +12,94 @@ import { UpdateProductDto } from './dto/update-product.dto';
 export class ProductsService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  create(createProductDto: CreateProductDto) {
-    const product = this.prismaService.produto.create({
-      data: {
-        ...createProductDto,
-      },
-    });
+  async findById(id: number) {
+    try {
+      const product = await this.prismaService.produto.findUnique({
+        where: { id },
+      });
 
-    return product;
+      if (!product) {
+        throw new NotFoundException(`Product with ID ${id} not found`);
+      }
+
+      return product;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new BadRequestException('Error fetching product: ' + error.message);
+    }
   }
 
-  findAll() {
-    return this.prismaService.produto.findMany();
+  async create(createProductDto: CreateProductDto) {
+    try {
+      const productAlreadyExists = await this.prismaService.produto.findFirst({
+        where: {
+          nome: createProductDto.nome,
+        },
+      });
+
+      if (productAlreadyExists) {
+        throw new ConflictException('Product with this name already exists');
+      }
+
+      const product = await this.prismaService.produto.create({
+        data: {
+          ...createProductDto,
+        },
+      });
+
+      return product;
+    } catch (error) {
+      throw new BadRequestException('Error creating product: ' + error.message);
+    }
   }
 
-  findOne(id: number) {
-    return this.prismaService.produto.findUnique({
-      where: {
-        id,
-      },
-    });
+  async findAll() {
+    try {
+      const products = await this.prismaService.produto.findMany();
+      return products;
+    } catch (error) {
+      throw new BadRequestException(
+        'Error fetching products: ' + error.message,
+      );
+    }
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    const product = this.prismaService.produto.update({
-      where: {
-        id,
-      },
-      data: {
-        ...updateProductDto,
-      },
-    });
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    try {
+      await this.findById(id);
 
-    return product;
+      const product = await this.prismaService.produto.update({
+        where: {
+          id,
+        },
+        data: {
+          ...updateProductDto,
+        },
+      });
+
+      return product;
+    } catch (error) {
+      if (error instanceof NotFoundException) {
+        throw error;
+      }
+      throw new BadRequestException('Error updating product: ' + error.message);
+    }
   }
 
-  remove(id: number) {
-    return this.prismaService.produto.delete({
-      where: {
-        id,
-      },
-    });
+  async remove(id: number) {
+    try {
+      await this.findById(id);
+
+      const product = await this.prismaService.produto.delete({
+        where: {
+          id,
+        },
+      });
+      return product;
+    } catch (error) {
+      throw new BadRequestException('Error deleting product: ' + error.message);
+    }
   }
 }
